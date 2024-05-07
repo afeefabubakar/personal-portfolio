df9fce42007d153cccf687a97564ddcf8689ac81
Afeef <contact@afeef.dev>
2024-05-07 11:10:06 +0800

    feat: hero bg image

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,12 @@
 var heroTitle = document.querySelector('.hero-title');
 var heroGreeting = document.querySelector('.hero-greetings');
-if (heroTitle && heroGreeting) {
-    heroGreeting.classList.add('fade-in');
-    heroGreeting.addEventListener('animationend', function () {
-        heroTitle.classList.add('slide-fade-in-right');
+var heroBg = document.querySelector('.hero-bg');
+if (heroBg && heroTitle && heroGreeting) {
+    heroBg.classList.add('dim');
+    heroBg.addEventListener('animationend', function () {
+        heroGreeting.classList.add('fade-in');
+        heroGreeting.addEventListener('animationend', function () {
+            heroTitle.classList.add('slide-fade-in-right');
+        });
     });
 }
--- scripts/hero.ts
+++ scripts/hero.ts
@@ -1,10 +1,14 @@
 const heroTitle: HTMLElement | null = document.querySelector('.hero-title');
 const heroGreeting: HTMLElement | null =
   document.querySelector('.hero-greetings');
+const heroBg: HTMLElement | null = document.querySelector('.hero-bg');
 
-if (heroTitle && heroGreeting) {
-  heroGreeting.classList.add('fade-in');
-  heroGreeting.addEventListener('animationend', () => {
-    heroTitle.classList.add('slide-fade-in-right');
+if (heroBg && heroTitle && heroGreeting) {
+  heroBg.classList.add('dim');
+  heroBg.addEventListener('animationend', () => {
+    heroGreeting.classList.add('fade-in');
+    heroGreeting.addEventListener('animationend', () => {
+      heroTitle.classList.add('slide-fade-in-right');
+    });
   });
 }
