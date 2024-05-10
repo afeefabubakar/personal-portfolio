910d0a62be43b7d83e1efc5605476a32a84df8a3
Afeef <contact@afeef.dev>
2024-05-10 15:09:34 +0800

    feat: personal value section

--- dist/app.js
+++ dist/app.js
@@ -1,24 +1,5 @@
-var header = document.querySelector('header');
-var backToTopButton = document.querySelector('sl-button.back-to-top');
-var backToTopRoot = customElements
-    .whenDefined('sl-button')
-    .then(function () { return document.querySelector('sl-button.back-to-top').shadowRoot; });
-backToTopRoot.then(function (root) {
-    var backToTopRootButton = root.querySelector('button');
-    backToTopRootButton.addEventListener('click', function () {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
-    var headerObserver = new IntersectionObserver(function (entries) {
-        if (entries[0].isIntersecting) {
-            backToTopRootButton.style.display = 'none';
-        }
-        else {
-            backToTopRootButton.style.display = 'flex';
-        }
-    });
-    headerObserver.observe(header);
-});
 var heroTitle = document.querySelector('.hero-title');
+var heroSubtitle = document.querySelector('.hero-subtitle');
 var heroGreeting = document.querySelector('.hero-greetings');
 var heroBg = document.querySelector('.hero-bg');
 if (heroBg && heroTitle && heroGreeting) {
@@ -27,6 +8,7 @@ if (heroBg && heroTitle && heroGreeting) {
         heroGreeting.classList.add('fade-in');
         heroGreeting.addEventListener('animationend', function () {
             heroTitle.classList.add('slide-fade-in-right');
+            heroSubtitle === null || heroSubtitle === void 0 ? void 0 : heroSubtitle.classList.add('fade-in');
         });
     });
 }
